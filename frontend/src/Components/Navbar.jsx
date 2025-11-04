ca82e056574f46d3c3bc2e4fe62f5b6a82f5ed7d
IamFJ03 <130732739+IamFJ03@users.noreply.github.com>
2025-11-04 23:15:22 +0530

    Meal Plans Page

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -7,23 +7,23 @@ export default function Navbar() {
     <div className='sticky top-0 z-50'>
       <div className='flex items-center justify-between px-20 py-5 bg-linear-to-r from-blue-200 to-white shadow-2xl'>
         <div>
-            <p className='font-bold text-2xl'><Link to={'/'}>FlavorFinds</Link></p>
+          <p className='font-bold text-2xl'><Link to={'/'}>FlavorFinds</Link></p>
         </div>
         <div>
-            <ul className='flex gap-20 items-center'>
-                <li className='cursor-pointer  transition-all duration-500 hover:text-blue-200'><select className="appearance-none border border-gray-300 rounded-lg py-2 pl-3 pr-10 text-gray-700 leading-tight focus:outline-none focus:border-blue-500">
-                  <option>Categories</option>
-                  <option>Course</option>
-                  <option>Cuisine</option>
-                  <option>Dietary Preferences</option>
-                  </select></li>
-                <li className='cursor-pointer hover:border-b-2 hover:border-blue-200 transition duration-500 hover:text-blue-200'><Link to={'/meal-plan'}>Meal Plans</Link></li>
-                <li className='cursor-pointer hover:border-b-2 hover:border-blue-200 transition duration-500 hover:text-blue-200'><Link to={'/favourites'}>My Favourites</Link></li>
-            </ul>
+          <ul className='flex gap-20 items-center'>
+            <li className=' transition-all duration-500 hover:text-blue-200'><select className="cursor-pointer appearance-none border border-gray-300 rounded-lg py-2 pl-3 pr-10 text-gray-700 leading-tight focus:outline-none focus:border-blue-500">
+              <option>Categories</option>
+              <option>Course</option>
+              <option>Cuisine</option>
+              <option>Dietary Preferences</option>
+            </select></li>
+            <li className='cursor-pointer hover:border-b-2 hover:border-blue-200 transition duration-500 hover:text-blue-200'><Link to={'/meal-plan'}>Meal Plans</Link></li>
+            <li className='cursor-pointer hover:border-b-2 hover:border-blue-200 transition duration-500 hover:text-blue-200'><Link to={'/favourites'}>My Favourites</Link></li>
+          </ul>
         </div>
         <div className='flex gap-3 items-center'>
-        <img src={user} className='w-8 h-8 rounded-full'/>
-        <button className='bg-blue-200 py-2 px-5 rounded-2xl cursor-pointer hover:shadow-xl transition-all duration-500 hover:scale-105'><Link to={'/Auth'}>Login/Sign Up</Link></button>
+          <img src={user} className='w-8 h-8 rounded-full' />
+          <button className='bg-blue-200 py-2 px-5 rounded-2xl cursor-pointer hover:shadow-xl transition-all duration-500 hover:scale-105'><Link to={'/Auth'}>Login/Sign Up</Link></button>
         </div>
       </div>
     </div>
--- frontend/src/Pages/Planning.jsx
+++ frontend/src/Pages/Planning.jsx
@@ -8,9 +8,69 @@ export default function Planning() {
     <div>
       <Navbar />
       <HeroSection path={currScreen} />
-      <div className='px-20 mt-10'>
-      <p className='text-xl'>Tailor-made meal plans designed for your health goals and lifestyle.</p>
-      <p className='text-xl'>Delicious, easy, and nutriciest approved</p>
+      <div>
+        <div className='px-20 mt-10'>
+        <p className='text-xl'>Tailor-made meal plans designed for your health goals and lifestyle.</p>
+        <p className='text-xl'>Delicious, easy, and nutriciest approved</p>
+        <p className='text-2xl font-bold my-10'>Filter By:</p>
+        </div>
+        <div className='flex items-center justify-between px-20'>
+          <div>
+            <p className='mb-2'>Goal</p>
+            <select name="Goal" className='appearance-none border border-gray-400 pl-3 pr-7 rounded py-1.75 focus:outline-none focus:border-blue-400'>
+              <option>Loss Weight</option>
+              <option>Maintain Weight</option>
+              <option>Gain Weight</option>
+            </select>
+          </div>
+          <div>
+            <p className='mb-2'>Diet</p>
+
+            <select name="Diet" className='appearance-none border border-gray-400 pl-3 pr-7 rounded py-1.75 focus:outline-none focus:border-blue-400'>
+              <option value="">Keto-Friendly</option>
+              <option value="">Paleo</option>
+              <option value="">Mediterranean</option>
+              <option value="">High Protein</option>
+            </select>
+          </div>
+          <div>
+            <p className='mb-2'>Dietary Type</p>
+            <select name="Type" className='appearance-none border border-gray-400 pl-3 pr-7 rounded py-1.75 focus:outline-none focus:border-blue-400'>
+              <option value="">Gluten-Free</option>
+              <option value="">Dairy-Free</option>
+              <option value="">Vegan</option>
+              <option value="">Vegetarian</option>
+            </select>
+          </div>
+          <div className='space-y-2'>
+            <p className='mb-2'>Nutrient Targets</p>
+
+            <select name="MacroNutrient" className='appearance-none border border-gray-400 pl-3 pr-7 rounded py-1.75 focus:outline-none focus:border-blue-400'>
+              <option value="">Protein</option>
+              <option value="">Carbs</option>
+              <option value="">Fat</option>
+              <option value="">Calories</option>
+            </select>
+          </div>
+
+          <div className='space-y-2'>
+            <p className='opacity-0'>_</p>
+            <select name="HealthNutrient" className='appearance-none border border-gray-400 pl-3 pr-7 rounded py-1.75 focus:outline-none focus:border-blue-400'>
+              <option value="">Sodium</option>
+              <option value="">Sugar</option>
+              <option value="">Potassium</option>
+              <option value="">Fiber</option>
+            </select>
+          </div>
+
+          <div className='space-y-2'>
+            <p className='opacity-0'>_</p> 
+            <select name="FatsQuality" className='appearance-none border border-gray-400 pl-3 pr-7 rounded py-1.75 focus:outline-none focus:border-blue-400'>
+              <option value="">Saturated Fat</option>
+              <option value="">Cholesterol</option>
+            </select>
+          </div>
+        </div>
       </div>
     </div>
   )
