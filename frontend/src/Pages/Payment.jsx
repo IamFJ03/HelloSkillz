e2bac9fdfb6db15829ea64f59c5d45ccec8e43ca
IamFJ03 <130732739+IamFJ03@users.noreply.github.com>
2025-12-05 22:58:57 +0530

    Payment UI

--- frontend/src/Pages/Payment.jsx
+++ frontend/src/Pages/Payment.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import Navbar from '../Components/Navbar';
-import { Currency } from 'lucide-react';
+import { Check } from 'lucide-react';
 
 export default function Payment() {
     const RAZORPAY_KEY_ID = 'rzp_test_RTJBBXdu6qLigb';
@@ -80,9 +80,9 @@ export default function Payment() {
     }
 
     const info = [
-        { heading: "Monthly Plan", amount: 9.99, for: "month", subhead1: "Ad-free browsing recipes", subhead2: "Exclusive recipe Collections", subHead3: "Advanced Filter Options", buttonTag: "Subscribe Monthly" },
-        { heading: "Annual Plan", amount: 99.99, for: "year", subhead1: "Unlimited Saved Recipes", subhead2: "Exclusive Recipe Collections", subHead3: "Advanced Filter Options", buttonTag: "Subscribe Annually" },
-        { heading: "Lifetime Plan", amount: 249.99, for: "Lifetime", subhead1: "All Annual Plan benefits", subhead2: "Future updates Included", subHead3: "Priority Support", buttonTag: "Buy Lifetime" }
+        { heading: "Monthly Plan", amount: 9.99, for: "month", subHead1: "Ad-free browsing recipes", subHead2: "Exclusive recipe Collections", subHead3: "Advanced Filter Options", buttonTag: "Subscribe Monthly" },
+        { heading: "Annual Plan", amount: 99.99, for: "year", subHead1: "Unlimited Saved Recipes", subHead2: "Exclusive Recipe Collections", subHead3: "Advanced Filter Options", buttonTag: "Subscribe Annually" },
+        { heading: "Lifetime Plan", amount: 249.99, for: "Lifetime", subHead1: "All Annual Plan benefits", subHead2: "Future updates Included", subHead3: "Priority Support", buttonTag: "Buy Lifetime" }
     ]
 
     return (
@@ -92,12 +92,25 @@ export default function Payment() {
                 <p className='font-sans text-4xl font-bold '>Unlock Premium Flavors</p>
                 <p className='text-lg mt-2'>Elevate Your culinary Journey with exclusive benefits.</p>
             </div>
-            <div className='flex  justify-between px-20'>
-                {info.map((data, index) => {
-                    <div className='h-70 w-100 bg-white shadow-md rounded-2xl'>
-                        <p>{data.heading}</p>
+            <div className='flex justify-between px-20 mt-10'>
+                {info.map((data, index) =>  (
+                    <div className='h-70 w-100 bg-white shadow-md rounded-2xl text-center hover:scale-110 hover:-mt-5 border border-transparent hover:border-blue-400 cursor-pointer transition-all duration-500'>
+                        <p className='text-lg font-bold text-center mt-3'>{data.heading}</p>
+                        <p className='text-2xl font-bold'>${data.amount}/{data.for}</p>
+                        <div className='my-5 text-left px-20'>
+                            <div className='flex items-center gap-2'>
+                        <Check size={20} color='black'/><p>{data.subHead1}</p>
+                        </div>
+                        <div className='flex items-center gap-2'>
+                        <Check size={20} color='black'/><p>{data.subHead2}</p>
+                        </div>
+                        <div className='flex items-center gap-2'>
+                        <Check size={20} color='black'/><p>{data.subHead3}</p>
+                        </div>
+                        </div>
+                        <button className='text-white bg-blue-200 px-18 py-1.5 rounded-lg cursor-pointer'>{data.buttonTag}</button>
                     </div>
-                })}
+                ))}
             </div>
 
             <button type="submit" disabled={isLoading} className='bg-[#5cb85c] rounded text-white py-2'>
